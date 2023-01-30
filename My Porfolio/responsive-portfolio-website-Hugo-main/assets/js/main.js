87c4aa72a4356295e5b592f95f1eaf56a20fe41e
Hc-Sky <60477108+Hc-Sky@users.noreply.github.com>
2023-01-30 16:28:37 +0100

    HTML, CSS & JS qualification

--- My Porfolio/responsive-portfolio-website-Hugo-main/assets/js/main.js
+++ My Porfolio/responsive-portfolio-website-Hugo-main/assets/js/main.js
@@ -44,13 +44,29 @@ function toggleSkills(){
         this.parentNode.className = 'skills__content skills__open'
     }
 }
-
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabsContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click',() =>{
+        const target = document.querySelector(tab.dataset.target)
 
+        tabsContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 /*==================== SERVICES MODAL ====================*/
 
 
