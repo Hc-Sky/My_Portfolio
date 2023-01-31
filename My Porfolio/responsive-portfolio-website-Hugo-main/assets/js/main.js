676c84ee52205193f9bf941a68b275311dd2e01b
Hc-Sky <60477108+Hc-Sky@users.noreply.github.com>
2023-01-31 14:53:51 +0100

    HTML CSS & JS Portfolio

--- My Porfolio/responsive-portfolio-website-Hugo-main/assets/js/main.js
+++ My Porfolio/responsive-portfolio-website-Hugo-main/assets/js/main.js
@@ -95,8 +95,19 @@ modalCloses.forEach((modalClose, i)=>{
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
-2
+let swiper = new Swiper(".portfolio__container", {
+      cssMode: true,
+      loop: true,
+      
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable : true,
+      },
+    });
 /*==================== TESTIMONIAL ====================*/
 
 
